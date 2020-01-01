0305d2af9053b1501b3ddf6efb5d6e0c5cf6a676
kristiandrex <cristiantorres.87@hotmail.com>
2020-01-01 15:45:57 -0500

    Pequeñas mejoras en el diseño y agregando el cálculo y almacenamiento del precio por cada vehículo

---
+++ src/hooks/usePrice.js
@@ -0,0 +1,24 @@
+import { useState, useContext } from "react";
+import { StorageContext } from "../components/App";
+
+export default function usePrice(type, minutes, hours) {
+	const { config } = useContext(StorageContext);
+	const defaultPrice = type === "CAR" ? config.priceCars : config.priceMotorcycles;
+
+	const [state, setState] = useState(() => {
+		return minutes + hours * 60 < config.requiredTime
+			? config.requiredTime * defaultPrice
+			: (minutes + hours * 60) * defaultPrice;
+	});
+
+	const setPrice = (minutes, hours) => {
+		const price =
+			minutes + hours * 60 < config.requiredTime
+				? config.requiredTime * defaultPrice
+				: (minutes + hours * 60) * defaultPrice;
+
+		setState(price);
+	};
+
+	return [state, setPrice];
+}
