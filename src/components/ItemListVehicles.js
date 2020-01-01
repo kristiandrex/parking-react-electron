f5cb8c9eb5cc3c855766a0e77766924004fd627a
kristiandrex <cristiantorres.87@hotmail.com>
2020-01-01 15:45:57 -0500

    Pequeñas mejoras en el diseño y agregando el cálculo y almacenamiento del precio por cada vehículo

--- src/components/ItemListVehicles.js
+++ src/components/ItemListVehicles.js
@@ -1,20 +1,27 @@
 import React, { useState, useEffect, useContext } from "react";
 import { StorageContext } from "./App";
+import usePrice from "../hooks/usePrice";
 
 export default function ItemListVehicles(props) {
 	const { vehicles, setVehicles } = useContext(StorageContext);
 
 	const [seconds, setSeconds] = useState(props.vehicle.seconds || 0);
-	let minutes = parseInt(seconds / 60);
-	let hours = parseInt(minutes / 60);
+	const minutes = parseInt(seconds / 60);
+	const hours = parseInt(minutes / 60);
+
+	const [price, setPrice] = usePrice(props.vehicle.type, minutes, hours);
 
 	useEffect(() => {
 		const timer = setInterval(() => {
 			setSeconds(seconds + 1);
+			setPrice(minutes, hours);
 
-			vehicles.find(
+			let vehicle = vehicles.find(
 				(vehicleStored) => vehicleStored.vehicleID === props.vehicle.vehicleID
-			).seconds = seconds + 1;
+			);
+
+			vehicle.seconds = seconds + 1;
+			vehicle.price = price;
 
 			setVehicles(vehicles);
 		}, 1000);
@@ -36,9 +43,11 @@ export default function ItemListVehicles(props) {
 			<td>{props.vehicle.ownerID}</td>
 			<td>{props.vehicle.phone}</td>
 			<td className="time">
-				{hours >= 10 ? hours : `0${hours}`}:{minutes >= 10 ? minutes : `0${minutes}`}
+				{hours >= 10 ? hours : `0${hours}`}:
+				{minutes % 60 >= 10 ? minutes % 60 : `0${minutes % 60}`}:
+				{seconds % 60 >= 10 ? seconds % 60 : `0${seconds % 60}`}
 			</td>
-			<td className="price">{props.vehicle.price}</td>
+			<td className="price">${price}</td>
 			<td>
 				<i
 					className="material-icons clear"
